8d8c1c7db198c1181077d233804917effe21d23f
nitaisutradhar <softbeezoffice@gmail.com>
2025-08-15 14:14:27 +0600

    remove header and add header name over videosection and The two buttons are pushed down

--- src/app/components/VideoSection.tsx
+++ src/app/components/VideoSection.tsx
@@ -11,7 +11,12 @@ const VideoSection = ({ onOpenModal }: { onOpenModal: () => void }) => (
       Your browser does not support the video tag.
     </video>
     <div className="absolute inset-0 flex flex-col items-center justify-center">
-        <div style={{ margin: "30px" }} className="cta-buttons md:flex gap-5">
+      {/* Brand Name */}
+      <div className="absolute top-10">
+        <a href="#" className="logo">VOUTCH</a>
+      </div>
+      {/* Buttons */}
+      <div style={{ margin: "30px" }} className="absolute bottom-1/5 cta-buttons md:flex gap-5">
         <button style={{ margin: "5px" }} className="btn btn-primary" onClick={onOpenModal}>Buy a Ticket</button>
         <button style={{ margin: "5px" }} className="btn btn-secondary" onClick={onOpenModal}>Sell a Ticket</button>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,6 @@ export default function Home() {
 
   return (
     <>
-      <Header />
       <VideoSection onOpenModal={() => setModalOpen(true)} />
       <main>
         <Hero />
